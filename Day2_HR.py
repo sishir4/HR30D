287dd0849caf1d15f22b8c08340dab29e0649094
sishir4 <sishirc17@gmail.com>
2019-08-02 22:27:57 -0700

    Update Day2_HR.py

--- Day2_HR.py
+++ Day2_HR.py
@@ -1,20 +1,13 @@
-#meal cost
-# tip cost
-# tax cost
+#HackerRank Day 2 Challenge with and without using functions in Python
+# The Strings have been customized, so please feel free to remove them.
 
-
-# meal = float(input('Please enter the price of the item: '))
-# tip = int(input('Please enter the tip %: '))
-# tax = int(input('Please enter the tax percentage: '))
-#
-# final_cost = meal +(tip/100)*meal + (tax/100)*meal
-
+meal = float(input('Please enter the price of the item: '))
+tip = int(input('Please enter the tip %: '))
+tax = int(input('Please enter the tax percentage: '))
+final_cost = meal +(tip/100)*meal + (tax/100)*meal
 print(round(final_cost))
 
-
-
-
-
+#Using functions
 def total_cost():
     meal = float(input('Please enter the price of the item: '))
     tip = int(input('Please enter the tip %: '))
