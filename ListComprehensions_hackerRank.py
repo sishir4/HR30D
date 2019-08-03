49756723f628c5d8ac7b7dd0f12664fb9ef6b305
sishir4 <sishirc17@gmail.com>
2019-08-02 23:33:03 -0700

    Update ListComprehensions_hackerRank.py

--- ListComprehensions_hackerRank.py
+++ ListComprehensions_hackerRank.py
@@ -1,18 +1,3 @@
-# def is_leap(year):
-#     if (year % 4 == 0) and (year % 100 != 0):
-#       # Note that in your code the condition will be true if it is not..
-#       leap = True
-#     elif (year % 100 == 0) and (year % 400 != 0):
-#       leap = False
-#     elif (year % 400 == 0):
-#       # For some reason here you had False twice
-#       leap = True
-#     else:
-#       leap = False
-#
-#     return leap
-# year = int(input('Type here: >> '))
-# print(is_leap(year))
 x = int(input('>'))
 y = int(input('>'))
 z = int(input('>'))
